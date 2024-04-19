ecc0a1ef30632c022eacbcdc92a3404d6ab347d6
Mostafa Aziz Zuher <thecool4a@gmail.com>
2024-04-19 16:01:25 +0200

    Added changeMode optimization

--- AFRY-3D-Visualizer/Assets/Scripts/UI scripts/MiningSiteChangeMode.cs
+++ AFRY-3D-Visualizer/Assets/Scripts/UI scripts/MiningSiteChangeMode.cs
@@ -4,13 +4,26 @@ using UnityEngine;
 
 public class MiningSiteChangeMode : MonoBehaviour
 {
+    private MeshRenderer meshRenderer1;
+    private MeshRenderer meshRenderer2;
 
+    private void Start()
+    {
+        MeshRenderer[] meshRenderers = this.gameObject.GetComponentsInChildren<MeshRenderer>();
+
+        meshRenderer1 = meshRenderers[0];
+        meshRenderer2 = meshRenderers[1];
+    }
     public void TurnOffMiningSite()
     {
-        this.gameObject.SetActive(false);
+        //this.gameObject.SetActive(false);
+        meshRenderer1.enabled = false;
+        meshRenderer2.enabled = false;
     }
     public void TurnOnMiningSite()
     {
-        this.gameObject.SetActive(true);
+        meshRenderer1.enabled = true;
+        meshRenderer2.enabled = true;
+
     }
 }
