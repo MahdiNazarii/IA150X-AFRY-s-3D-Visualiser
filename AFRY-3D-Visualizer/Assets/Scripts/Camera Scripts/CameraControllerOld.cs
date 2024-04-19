843de85fc4d7bfba70fde96af0f1d7e9671a5b77
Mahdi Nazari <100143248+MahdiNazarii@users.noreply.github.com>
2024-04-19 18:26:50 +0200

    Some refactoring of the code

--- AFRY-3D-Visualizer/Assets/Scripts/Camera Scripts/CameraControllerOld.cs
+++ AFRY-3D-Visualizer/Assets/Scripts/Camera Scripts/CameraControllerOld.cs
@@ -13,7 +13,6 @@ public class CameraControllerOld : MonoBehaviour
     public float zoomSpeed = 2.0f;
     private Vector2 rotation = Vector2.zero;
 
-    public bool canMove = true;
 
     private void Awake()
     {
@@ -22,39 +21,21 @@ public class CameraControllerOld : MonoBehaviour
     }
     private void Update()
     {
-        //CursorMode =  CursorLockMode.Locked;
-        if(canMove)
-        {
-            RotateCamera();
-            ZoomCamera();
+        RotateCamera();
+        ZoomCamera();
            
-        }
+        
     }
 
 
     private void RotateCamera()
     {
-         rotation.y += Input.GetAxis("Mouse X") * rotationSpeed;
-         rotation.x += -Input.GetAxis("Mouse Y") * rotationSpeed;
+        rotation.y += Input.GetAxis("Mouse X") * rotationSpeed;
 
-        //rotation.x = Mathf.Clamp(rotation.x, -90f, 90f);
+      
         //rotates the camera target according to lateral mouse movement
         this.gameObject.transform.localRotation = Quaternion.Euler(0, rotation.y, 0);
-        //Debug.Log(this.transform.rotation.ToString());
         transform.eulerAngles = new Vector3(0, rotation.y, 0);
-
-        //float mouseX = Input.GetAxis("Mouse X");
-        //float mouseY = Input.GetAxis("Mouse Y");
-
-        // Rotate around Y axis
-        //transform.Rotate(Vector3.up, mouseX * rotationSpeed, Space.World); // Use Space.World to rotate around global Y axis
-        //
-        //// Rotate around X axis
-        //Quaternion cameraRotation = virtualCamera.transform.localRotation * Quaternion.Euler(-mouseY * rotationSpeed, 0f, 0f); // Calculate new rotation
-        //float angleX = cameraRotation.eulerAngles.x;
-        //if (angleX > 180f) angleX -= 360f; // Convert angle to -180 to 180 range
-        //float newAngleX = Mathf.Clamp(angleX, -80f, 80f); // Clamp rotation to prevent flipping
-        //virtualCamera.transform.localRotation = Quaternion.Euler(newAngleX, 0f, 0f); // Apply new rotation
 
     }
 
